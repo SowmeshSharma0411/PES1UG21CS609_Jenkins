578bf7a57690683c829be1528362951cf30a67b5
Sowmesh <sowmeshshailesh1234@gmail.com>
2024-03-08 16:02:04 +0530

    Task 2 commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins I am PES1UG21CS609\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
