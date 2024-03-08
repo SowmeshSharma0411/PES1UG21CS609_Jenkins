26ee9dc67bde4d77f7acca2b01873c3c8e755a5c
Sowmesh <sowmeshshailesh1234@gmail.com>
2024-03-08 16:52:52 +0530

    Added webhook!

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins I am PES1UG21CS609\n";
   cout << "I have successfully built and run\n";
-  cout << "I have successfully enabled the Webhook\n";
+  cout << "I have successfully enabled the Webhook!\n";
   return 0;
 }
